a6cb25ee48e1e3b8d088e7cb65072b4e466769e7
Anubhav Panda <anubhavpanda2@gmail.com>
2018-09-27 18:57:56 -0500

    demoPubsubmodel

---
+++ publisher/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.ServiceBus.Messaging;
+namespace publisher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sbconnstring = "connectionString";
+            string queuename = "myq";
+            QueueClient client = QueueClient.CreateFromConnectionString(sbconnstring, queuename);
+            BrokeredMessage message = new BrokeredMessage("this is test message 1!");
+            client.Send(message);
+            Console.WriteLine("message sent successfully");
+            Console.Read();
+
+        }
+    }
+}
---
+++ subscriber/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.ServiceBus.Messaging;
+namespace subscriber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sbconnstring = "connectionstring";
+            string queuename = "myq";
+            QueueClient client = QueueClient.CreateFromConnectionString(sbconnstring, queuename);
+            client.OnMessage(msg => {
+                Console.WriteLine($"{msg.MessageId} body {msg.GetBody<string>()}");
+            });
+            Console.ReadKey();
+        }
+    }
+}
